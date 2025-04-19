e2480c7469450e66e922aba939005e5138db410b
eric <504068014080@tutanota.com>
2025-04-19 22:25:20 +0900

    intext comments removed

--- model.py
+++ model.py
@@ -6,16 +6,7 @@ from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 def create_faster_rcnn_model(num_classes):
-    """
-    Create and return a Faster R-CNN model with a custom number of classes.
-    The first class is the background, so the total number of classes will be num_classes + 1.
-    
-    Args:
-        num_classes (int): Number of custom classes in the dataset (excluding background).
-    
-    Returns:
-        model (FasterRCNN): Pretrained Faster R-CNN model with modified classifier.
-    """
+
     # Load the pre-trained Faster R-CNN model with a ResNet-50 backbone
     model = fasterrcnn_resnet50_fpn(pretrained=True)
 
